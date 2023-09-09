update to fix FD to IDR masking and minor bugs

finches/PDB_structure_tools.py
@@ -12,6 +12,7 @@ updated: 2022-07-22
 from soursop.sstrajectory import SSTrajectory
 import metapredict
 import math
+import numpy as np
 
 from .data.reference_sequence_info import GS_seqs_dic
 
@@ -190,14 +191,16 @@ def build_column_mask_based_on_xyz(matrix, SAFD_cords, IDR_positon=['Cterm','Nte
         polymer limits of equiviant length polymer relitive to the position of the residue 
         in the IDR and where the IDR is attached to the folded domain.
 
+    NOTE THIS IS BROKEN
+
     """
 
     # get the xyz position of where the IDR attaches to the FD
     if IDR_positon == 'Cterm':
-        IDR0_xyz == SAFD_cords[-1]
+        IDR0_xyz = SAFD_cords[-1]
 
     elif IDR_positon == 'Nterm':
-        IDR0_xyz == SAFD_cords[0]
+        IDR0_xyz = SAFD_cords[0]
 
     out_matrix = []
     # for each SAFD residue row
@@ -210,13 +213,14 @@ def build_column_mask_based_on_xyz(matrix, SAFD_cords, IDR_positon=['Cterm','Nte
         # for each IDR residue col
         for i, v in enumerate(col):
             if SAFD_distance < GS_seqs_dic[i]:
-                l_out_row.append(0)
-            else:
                 l_out_row.append(1)
+            else:
+                l_out_row.append(0)
+
         out_matrix.append(l_out_row)
 
     out_mask = np.array(out_matrix)
-    if out_matrix.shape() != matrix.shape():
+    if out_mask.shape != np.array(matrix).shape:
         raise Exception('New built mask does not match shape of passed matrix')
 
     return out_mask 
@@ -528,15 +532,16 @@ def map_SAFD_vector_to_full_folded_domain(partial_vector, SAFD_idxs, FD_start, F
     _check_bounds(SAFD_idxs, FD_start, FD_end)
 
     
-    FULL_FD_idxs = range(FD_start, FD_end+1)
+    FULL_FD_idxs = [i for i in range(FD_start, FD_end+1)]
     FULL_length = FD_end - FD_start + 1
     FULL_FD_vector = [null_value] * FULL_length
     
     # make full vector 
     for ei, i in enumerate(SAFD_idxs):
         if i in SAFD_idxs: 
-            FULL_FD_vector[i] = partial_vector[ei]
-    
+            l_i = i - FD_start
+            FULL_FD_vector[l_i] = partial_vector[ei]
+
     # check lengths 
     if len(FULL_FD_vector) != FULL_length and len(FULL_FD_vector) != FULL_FD_idxs:
         raise Exception(f'ERROR building new vectors lengths are not expected.') 
@@ -586,8 +591,8 @@ def extract_flanking_domain_combinations(pdb, return_domain_lists=False):
     l_seq = PO.get_amino_acid_sequence(oneletter=True)
 
     # get list of domains in found in PDB
-    disorder = metapredict.predict_disorder_domains(seq)
-    IDRS = disorder.disordered_domain_boundaries
+    disorder = metapredict.predict_disorder_domains(l_seq)
+    IDRs = disorder.disordered_domain_boundaries
     FDs = disorder.folded_domain_boundaries
     
     # find_flanking_combinations of IDRs and FDs 
@@ -598,7 +603,7 @@ def extract_flanking_domain_combinations(pdb, return_domain_lists=False):
                 flanking_combinations.append((idr, fd))
 
     if return_domain_lists:
-        return flanking_combinations, IDRS, FDs
+        return flanking_combinations, IDRs, FDs
     else:
         return flanking_combinations
 

finches/data/forcefeild_dependencies.py
@@ -6,7 +6,8 @@ By : Garrett M. Ginell & Alex S. Holehouse
 """
 
 from scipy.stats import linregress
-# from ..epsilon_calculation import get_sequence_epsilon_value, Interaction_Matrix_Constructor
+import finches
+import numpy as np
 
 ## ------------------------------------------------------------------------------
 ##
@@ -44,10 +45,9 @@ def _GS_length_generator(N, start_AA="G"):
         
     return ''.join(seq)
 
-
 ## ------------------------------------------------------------------------------
 ##
-def get_null_interaction_baseline(model, min_len=10, max_len=500):
+def get_null_interaction_baseline(X_model, min_len=10, max_len=500):
     """
     Function to arrive at null interaction baseline for specific passed model
     This works by testing a bunch of different null_interaction_baselines and 
@@ -58,8 +58,8 @@ def get_null_interaction_baseline(model, min_len=10, max_len=500):
     
     Parameters
     ---------------
-    model : obj
-        An instantiation of the one of the forcefield class objects 
+    X_model : obj
+        An instantiation of the Interaction_Matrix_Constructor class 
 
     min_len : int 
         The minimum length of a polyGS sequence used 
@@ -76,22 +76,20 @@ def get_null_interaction_baseline(model, min_len=10, max_len=500):
 
     """
 
-    # build constructor with random baseline
-    X_model = Interaction_Matrix_Constructor(model, null_interaction_baseline=0)
-
     null_baseline_values_dict = {}
-    base_lines = np.arange(-0.2,0,.001) # range may need to be updated
+    base_lines = np.arange(-0.2,0,.01) # range may need to be updated
 
     GS_refseq = {i: _GS_length_generator(i, start_AA="G") for i in range(min_len,max_len)}
     SG_refseq = {i: _GS_length_generator(i, start_AA="S") for i in range(min_len,max_len)}
 
     # iterate possible baselines
     for ibl in base_lines:
-
-        ref_null_interaction_baseline = ibl
        
-        GS_ref_dict = {i:get_sequence_epsilon_value(a, a, null_interaction_baseline = ref_null_interaction_baseline, X=model) for i,a in GS_refseq.items()}
-        SG_ref_dict = {i:get_sequence_epsilon_value(a, a, null_interaction_baseline = ref_null_interaction_baseline, X=model) for i,a in SG_refseq.items()}
+        # not charge prefactor can be set to 0 here because the null_interaction_baseline
+        # generation does not depend on any sequences containing charge residues 
+
+        GS_ref_dict = {i:finches.epsilon_calculation.get_sequence_epsilon_value(a, a, null_interaction_baseline=ibl, prefactor=0, X=X_model, CHARGE=False, ALIPHATICS=False) for i,a in GS_refseq.items()}
+        SG_ref_dict = {i:finches.epsilon_calculation.get_sequence_epsilon_value(a, a, null_interaction_baseline=ibl, prefactor=0, X=X_model, CHARGE=False, ALIPHATICS=False) for i,a in SG_refseq.items()}
 
         GS_avg_reference_dic = {i: (GS_ref_dict[i] + SG_ref_dict[i])/2 for i in SG_refseq}
 

finches/interaction_vector.py
@@ -6,6 +6,7 @@ between IDRs and other sequences and attached folded domains.
 By : Garrett M. Ginell & Alex S. Holehouse 
 2023-08-06
 """
+import numpy as np
 import matplotlib.pyplot as plt
 
 from .epsilon_calculation import get_sequence_epsilon_vectors, get_interdomain_epsilon_vectors
@@ -58,15 +59,15 @@ def show_folded_domain_interaction_on_sequence(pdb, FD_start, FD_end,
 
     if not title:
         if sequence_of_reff == 'sequence2':
-            title = f'''Interaction Vector of a {IDR_positon} {sequence_names[1]} relitve to residues\n
-                         on the surface of the folded domain ({FD_start}-{FD_end}) in {sequence_names[0]}\n 
-                        computed with the {pdb} pdb file and using the {X.parameters.version} model.'''
+            title = f'''Interaction Vector of a {IDR_positon} {sequence_names[1]} relitve to residues
+            on the surface of the folded domain ({FD_start}-{FD_end}) in {sequence_names[0]} 
+            computed with the pdb file:\n {pdb} \n and using the {X.parameters.version} model.'''
         else: 
-            title = f'''Interaction Vector of the surface residue on the folded domain ({FD_start}-{FD_end})\n
-                        in {sequence_names[0]} relitve to the {IDR_positon} {sequence_names[1]} \n
-                        computed with the {pdb} pdb file and using the {X.parameters.version} model.'''
+            title = f'''Interaction Vector of the surface residue on the folded domain ({FD_start}-{FD_end})
+            in {sequence_names[0]} relitve to the {IDR_positon} {sequence_names[1]}
+            computed with the pdb file:\n {pdb} \n and using the {X.parameters.version} model.'''
 
-    f = make_interaction_vector_plot(attractive_vector, repulsive_vector, sequence1, sequence_names=sequence_names, 
+    f = make_interaction_vector_plot(attractive_vector, repulsive_vector, sequence2, sequence_names=sequence_names, 
                                      title=title)
     return f
 
@@ -169,7 +170,7 @@ def make_interaction_vector_plot(attractive_vector, repulsive_vector, sequence1,
 
     ax.set_title(title)
     plt.xticks(np.arange(0,len(repulsive_vector)), sequence1, fontsize=6, ha="center")
-    ptl.yticks(fontsize=7)
+    plt.yticks(fontsize=7)
 
     return f
 

finches/parsing_aminoacid_sequences.py
@@ -114,7 +114,7 @@ def get_charge_weighed_FD_mask(sequence1, sequence2):
         if r1 in charges:
             for j,r2 in enumerate(sequence2):  
                 if r2 in charges: 
-                    l_resis = [r1] + get_neighboors_window_of3(j,sequence2) #this line is the difference HERE 
+                    l_resis = r1 + get_neighboors_window_of3(j,sequence2) #this line is the difference HERE 
                     
                     [local_fcr, local_ncpr] = calculate_FCR_and_NCPR(l_resis)
                     chrg_weight = np.abs(local_ncpr / local_fcr)