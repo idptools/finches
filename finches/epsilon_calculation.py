I fixed a lot of the bugs and cleaned up the code some. The huge improvement is the boltzmann based averaging in the stateless function set. This fixed my charge correction problems for IDR vs IDR interactions and gives the correct sign at least for two IDR-Structure domain interactions. The full test will run tonight.

finches/epsilon_calculation.py
@@ -1066,13 +1066,16 @@ class ArbitraryFilterInteractionMatrixContructor(InteractionMatrixConstructor):
                  null_interaction_baseline, 
                  compute_forcefield_dependencies)
         #initialize the weighting function to use for computation later
-        self.weight_function = weight_function
+        func1 = lambda x: np.exp(-1*np.power(x/(1*3.4),2)/2)
+        if weight_function is None:
+            self.weight_function = func1
+        else:
+            self.weight_function = weight_function
 
-        #define the charge correction weighting function
-        func1 = lambda x: np.exp(-1*np.power(x/(0.5*3.4),1)/2)
+        #define the charge correction weighting function (currently not used)
         self.weight_function_charge = func1
 
-        #define the aliphatic correction weighting function
+        #define the aliphatic correction weighting function (currently not used)
         self.weight_function_aliphatic = func1
         
     
@@ -1176,7 +1179,7 @@ class ArbitraryFilterInteractionMatrixContructor(InteractionMatrixConstructor):
         return res
 
     def _check_local_charge_criteria(self, charge_vec : np.ndarray, distance_vec : np.ndarray,
-                                    min_charge : int = 4, max_dist : float = 3.4) -> bool:
+                                    min_charge : int = 5, max_dist : float = 3.4) -> bool:
         '''Checks that there are at enough local charges'''
         #find all the charge states within max_dist
         close_charge = charge_vec[distance_vec <= max_dist]
@@ -1198,7 +1201,7 @@ class ArbitraryFilterInteractionMatrixContructor(InteractionMatrixConstructor):
         float
             This is the correction term to subtract off the 
         '''
-        param = 1
+        param = 0.2
 
         correction_term = 0 #this is the correction term to return (0 if the origin has no charged residues)
         #find all the letters that localize at 0 away from the origin
@@ -1345,7 +1348,8 @@ class ArbitraryFilterInteractionMatrixContructor(InteractionMatrixConstructor):
             pass #fix this later
             #ret_val = ret_val - self.calculate_aliphatic_correction_term(seq_concat, dist_concat)
         if use_charge_weighting:
-            ret_val = ret_val - self.calculate_charge_correction_term(seq_concat, dist_concat)
+            pass
+            #ret_val = ret_val - self.calculate_charge_correction_term(seq_concat, dist_concat)
         if offset:
             ret_val = ret_val - self.threshold_offset
         

finches/utils/folded_domain_utils.py
@@ -596,7 +596,8 @@ class FoldeDomain:
         Nick's Version
         This function calculates the surface epsilon values for each residue in the
         protein. The function will calculate the surface epsilon for each residue in 
-        the protein and return this in a surface_epsilon dictionary.
+        the protein and return this as a matrix. The matrix correlation back to the dataset
+        is based on self.surface_indices.
 
         Briefly, this function works by doing the following:
 
@@ -657,16 +658,9 @@ class FoldeDomain:
             [2] - the surface epsilon value for that residue.
 
         """
-        #amino acid constants
-        hydrophobes = ['I' , 'V', 'L', 'A', 'M']
-        negative = ['D', 'E']
-        positive = ['K', 'R']
         
         #recompute the neighbors based on distance specified
         self.get_nearest_neighbour_res(distance_thresh=window_struct_distance_extent)
-
-        #creating a blank for saving the result
-        surface_eps = {}
         
         #sequence distance factor
         seq_dist_factor = window_struct_distance_extent/window_seq_distance_extent
@@ -678,8 +672,6 @@ class FoldeDomain:
             adj_idr_idxs = np.arange(0,len(input_sequence))
             #settup the return matrix output (preallocate)
         ret_mat = np.zeros((len(self.surface_indices),len(input_sequence)), dtype=float)
-        if split: #if we split we need to pass twice the information back.
-            ret_mat2 = np.zeros((len(self.surface_indices),len(input_sequence)), dtype=float)
         #loop over the structure idxs and idr idxs
         for ll,struct_idx_center in enumerate(self.surface_indices):
             for idr_idx_center in adj_idr_idxs:
@@ -705,8 +697,7 @@ class FoldeDomain:
                 #grabe the distances
                 idr_local_distance = [seq_dist_factor*np.abs(k-idr_idx_center) for k in idr_negihbor_resid]
                 
-                ret_mat[ll,idr_idx_center] = IMCObject.calc_filtered_region(struct_str, struct_local_distance, idr_str, idr_local_distance,
-                                                                                split = True)
+                ret_mat[ll,idr_idx_center] = IMCObject.calc_filtered_region(struct_str, struct_local_distance, idr_str, idr_local_distance)
                     
         # #determine if the extra whitespace on the exterior of the vectors needs to get removed
         # if remove_extra_space:
@@ -722,7 +713,7 @@ class FoldeDomain:
         
         #combine the data if nessisary
         if split:
-            ret_mat = ((ret_mat>split_threshold)*ret_mat, (ret_mat<=split_threshold)*ret_mat2)
+            ret_mat = ((ret_mat>split_threshold)*ret_mat, (ret_mat<=split_threshold)*ret_mat)
         return ret_mat
         
 